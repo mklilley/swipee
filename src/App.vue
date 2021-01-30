1f66527762e4d0f0cc31351fd3611f3391b8460f
Matt Lilley <m.k.lilley@gmail.com>
2021-01-30 18:48:23 +0000

    Adds Swipable-cards code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="cards">
+      <Card
+        v-for="(card, index) in cards"
+        :key="card"
+        :card="card"
+        :is-current="index === 0"
+        @cardAccepted="handleCardAccepted"
+        @cardRejected="handleCardRejected"
+        @cardSkipped="handleCardSkipped"
+        @hideCard="removeCardFromDeck"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+
+  data() {
+    return {
+      cards: ["Test", "Vue.js", "Webpack"],
+    };
+  },
+
+  methods: {
+    handleCardAccepted() {
+      console.log("handleCardAccepted");
+    },
+    handleCardRejected() {
+      console.log("handleCardRejected");
+    },
+    handleCardSkipped() {
+      console.log("handleCardSkipped");
+    },
+    removeCardFromDeck() {
+      this.cards.shift();
+    },
+  },
+};
 </script>
 
-<style>
-#app {
+<style lang="scss">
+.cards {
+  position: relative;
+  display: flex;
+  margin: 50px;
+  width: 300px;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+@import "./styles/mixins.scss";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,261 @@
+<template>
+  <div
+    v-if="isShowing"
+    ref="interactElement"
+    :class="{
+      isAnimating: isInteractAnimating,
+      isCurrent: isCurrent,
+    }"
+    class="card"
+    :style="{ transform: transformString }"
+  >
+    <h3 class="cardTitle">{{ card }}</h3>
+  </div>
+</template>
+
+<script>
+import interact from "interactjs";
+const ACCEPT_CARD = "cardAccepted";
+const REJECT_CARD = "cardRejected";
+const SKIP_CARD = "cardSkipped";
+const HIDE_CARD = "hideCard";
+
+export default {
+  static: {
+    interactMaxRotation: 15,
+    interactOutOfSightXCoordinate: 500,
+    interactOutOfSightYCoordinate: 600,
+    interactYThreshold: 150,
+    interactXThreshold: 100,
+  },
+  emits: [HIDE_CARD, ACCEPT_CARD, REJECT_CARD, SKIP_CARD],
+  props: {
+    card: {
+      type: String,
+      required: true,
+    },
+    isCurrent: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isShowing: true,
+      isInteractAnimating: true,
+      isInteractDragged: null,
+      interactPosition: {
+        x: 0,
+        y: 0,
+        rotation: 0,
+      },
+    };
+  },
+
+  computed: {
+    transformString() {
+      if (!this.isInteractAnimating || this.isInteractDragged) {
+        const { x, y, rotation } = this.interactPosition;
+        return `translate3D(${x}px, ${y}px, 0) rotate(${rotation}deg)`;
+      }
+
+      return null;
+    },
+  },
+
+  mounted() {
+    const element = this.$refs.interactElement;
+
+    interact(element).draggable({
+      listeners: {
+        start: () => {
+          this.isInteractAnimating = false;
+        },
+        move: (event) => {
+          const {
+            interactMaxRotation,
+            interactXThreshold,
+          } = this.$options.static;
+          const x = this.interactPosition.x + event.dx;
+          const y = this.interactPosition.y + event.dy;
+
+          let rotation = interactMaxRotation * (x / interactXThreshold);
+
+          if (rotation > interactMaxRotation) rotation = interactMaxRotation;
+          else if (rotation < -interactMaxRotation)
+            rotation = -interactMaxRotation;
+
+          this.interactSetPosition({ x, y, rotation });
+        },
+        end: () => {
+          const { x, y } = this.interactPosition;
+          const {
+            interactXThreshold,
+            interactYThreshold,
+          } = this.$options.static;
+          this.isInteractAnimating = true;
+
+          if (x > interactXThreshold) this.playCard(ACCEPT_CARD);
+          else if (x < -interactXThreshold) this.playCard(REJECT_CARD);
+          else if (y > interactYThreshold) this.playCard(SKIP_CARD);
+          else this.resetCardPosition();
+        },
+      },
+    });
+  },
+
+  beforeUnmount() {
+    interact(this.$refs.interactElement).unset();
+  },
+
+  methods: {
+    hideCard() {
+      setTimeout(() => {
+        this.isShowing = false;
+        this.$emit(HIDE_CARD, this.card);
+      }, 300);
+    },
+
+    playCard(interaction) {
+      const {
+        interactOutOfSightXCoordinate,
+        interactOutOfSightYCoordinate,
+        interactMaxRotation,
+      } = this.$options.static;
+
+      this.interactUnsetElement();
+
+      switch (interaction) {
+        case ACCEPT_CARD:
+          this.interactSetPosition({
+            x: interactOutOfSightXCoordinate,
+            rotation: interactMaxRotation,
+          });
+          this.$emit(ACCEPT_CARD);
+          break;
+        case REJECT_CARD:
+          this.interactSetPosition({
+            x: -interactOutOfSightXCoordinate,
+            rotation: -interactMaxRotation,
+          });
+          this.$emit(REJECT_CARD);
+          break;
+        case SKIP_CARD:
+          this.interactSetPosition({
+            y: interactOutOfSightYCoordinate,
+          });
+          this.$emit(SKIP_CARD);
+          break;
+      }
+
+      this.hideCard();
+    },
+
+    interactSetPosition(coordinates) {
+      const { x = 0, y = 0, rotation = 0 } = coordinates;
+      this.interactPosition = { x, y, rotation };
+    },
+
+    interactUnsetElement() {
+      interact(this.$refs.interactElement).unset();
+      this.isInteractDragged = true;
+    },
+
+    resetCardPosition() {
+      this.interactSetPosition({ x: 0, y: 0, rotation: 0 });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/index.scss";
+
+$cardsTotal: 3;
+$cardsWidth: 300px;
+$cardsPositionOffset: 55vh * 0.06;
+$cardsScaleOffset: 0.08;
+$defaultTranslation: $cardsPositionOffset * $cardsTotal;
+$defaultScale: 1 - ($cardsScaleOffset * $cardsTotal);
+$fs-card-title: 1.125em;
+
+.card {
+  @include card();
+  @include absolute(0);
+  @include sizing(100% 80vw);
+  @include flex-center();
+
+  @include after() {
+    @include sizing(21px 3px);
+    @include absolute(right 0 bottom 11px left 0);
+
+    margin: auto;
+    border-radius: 100px;
+    background: rgba($c-black, 0.3);
+  }
+
+  display: flex;
+  max-height: 350px;
+  margin: auto;
+  font-size: $fs-h2;
+  font-weight: $fw-bold;
+  color: $c-white;
+  background-image: linear-gradient(
+    -180deg,
+    $primary-gradient-start 2%,
+    $primary-gradient-end 100%
+  );
+  opacity: 0;
+  transform: translateY($defaultTranslation) scale($defaultScale);
+  transform-origin: 50%, 100%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  user-select: none;
+  touch-action: none;
+  pointer-events: none;
+  will-change: transform, opacity;
+
+  height: 100vw;
+
+  &.isCurrent {
+    pointer-events: auto;
+  }
+
+  &.isAnimating {
+    transition: transform 0.7s $ease-out-back;
+  }
+}
+
+.cardTitle {
+  margin: 0 0 15px;
+  font-size: $fs-card-title;
+}
+
+@for $i from 1 through $cardsTotal {
+  $index: $i - 1;
+  $translation: $cardsPositionOffset * $index;
+  $scale: 1 - ($cardsScaleOffset * $index);
+
+  .card:nth-child(#{$i}) {
+    z-index: $cardsTotal - $index;
+    opacity: 1;
+    transform: translateY($translation) scale($scale);
+
+    @if $i == 3 {
+      color: $c-red-25;
+      background-color: $c-red-25;
+    } @else if $i == 2 {
+      color: $c-red-50;
+      background-color: $c-red-50;
+    }
+
+    @if $i != 1 {
+      background-image: none;
+
+      @include after() {
+        @include sizing(0 0);
+      }
+    }
+  }
+}
+</style>
