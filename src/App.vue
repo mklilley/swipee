f622e7297d84c6ba5c9f95f274231ad6eaa8f507
Matt Lilley <m.k.lilley@gmail.com>
2021-02-06 18:36:17 +0000

    Adds some global CSS for forms and buttons

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ export default {
     },
     openAddModal() {
       this.addModalVisible = true;
-  },
+    },
     openSettingsModal() {},
   },
 };
@@ -89,5 +89,51 @@ body {
   height: calc(
     100% - 140px
   ); // 140 = 2*70, 70 comes from TopBar component height
+}
+
+label {
+  font-weight: 400;
+  color: #333;
+  margin-right: 10px;
+}
+
+textarea {
+  font-size: 16px;
+}
+
+input {
+  border-radius: 5px;
+  border: 2px solid #eaeaea;
+  padding: 10px;
+  outline: none;
+  font-size: 16px;
+}
+
+button {
+  border-radius: 5px;
+  border: 1px solid #df1165;
+  background-color: #df1165;
+  padding: 8px 15px;
+  outline: none;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+button:disabled,
+button[disabled] {
+  border: 1px solid #999999 !important;
+  background-color: #cccccc !important;
+  color: #666666 !important;
+}
+
+button.copied::after {
+  content: " 👍";
+}
+
+button.wait::after {
+  content: " ⌛";
 }
 </style>
