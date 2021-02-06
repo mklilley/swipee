3c4588203c895d29ac8fa875ebd442189ba5e830
Matt Lilley <m.k.lilley@gmail.com>
2021-02-06 18:35:11 +0000

    Create AddCard component

---
+++ src/components/AddCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <Modal v-on:close="$emit('cancel')">
+    <template v-slot:body>
+      <h2>Add new link</h2>
+      <!--  -->
+      <div v-if="stage === 1">
+        <input
+          v-on:keypress.enter="next()"
+          v-model.trim="url"
+          type="text"
+          placeholder="Link goes here"
+          autocomplete="off"
+        />
+        <br /><br />
+        <span class="error" v-show="error"
+          >Oops! Flashcards need a front and a back.</span
+        >
+      </div>
+      <div v-if="stage === 2">
+        TODO: Read/Listen/Watch
+      </div>
+      <div v-if="stage === 3">
+        TODO: Short/Medium/Long
+      </div>
+      <button v-on:click="next()">Next</button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+
+export default {
+  name: "AddCard",
+  emits: ["cancel", "saved"],
+  components: {
+    Modal,
+  },
+  data() {
+    return { url: "", stage: 1 };
+  },
+  methods: {
+    next() {
+      this.stage += 1;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  width: 50%;
+}
+</style>
