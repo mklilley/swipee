765a9a6c350735b904527ebba2affcb1071e7f3f
Matt Lilley <m.k.lilley@gmail.com>
2021-03-15 23:18:10 +0000

    Payments server now stores user credits on jsonbox for validation

--- payments/index.js
+++ payments/index.js
@@ -6,31 +6,60 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 
-const stripe = require("stripe")(process.env.API_KEY_TEST);
+const stripe = require("stripe")(process.env.STRIPE_API_KEY_TEST);
+
+const db = require("./jsonbox.js");
 
 const PORT = process.env.PORT || 9000;
 
 const PRICES = JSON.parse(process.env.PRICES);
+const CREDITS = JSON.parse(process.env.CREDITS);
 
 app.use(cors());
 app.use(express.static("."));
 app.use(express.json());
 
-const calculateOrderAmount = (items) => {
+function calculateOrderAmount(items) {
   let totalCost = 0;
   for (const item of items) {
     totalCost += item.quantity * PRICES[item.code];
   }
   return totalCost;
-};
+}
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+function calculateCredits(items) {
+  let totalCredits = 0;
+  for (const item of items) {
+    totalCredits += item.quantity * CREDITS[item.code];
+  }
+  return totalCredits;
+}
 
-  // validate input
-  let itemsValidated = false;
+function authValidator(auth) {
+  let isValidated = false;
+
+  if (typeof auth === "object") {
+    if (auth.hasOwnProperty("apiKey") && auth.hasOwnProperty("boxID")) {
+      if (typeof auth.boxID === "string" && typeof auth.apiKey === "string") {
+        // Check that the user has entered a valid boxID, i.e.
+        // 20 character HEX string
+        let isHex20 = auth.boxID.match("^[0-9a-f]{20}$");
+        // Check that the user has entered a valid  valid apiKey i.e a UUID
+        let isUUID = auth.apiKey.match(
+          "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+        );
+        isValidated = !(isHex20 === null || isUUID === null);
+      }
+    }
+  }
+
+  return isValidated;
+}
+
+function itemsValidator(items) {
+  let isValidated = false;
   if (typeof items === "object" && items instanceof Array) {
-    itemsValidated = items.every((obj) => {
+    isValidated = items.every((obj) => {
       if (obj) {
         if (obj.hasOwnProperty("code") && obj.hasOwnProperty("quantity")) {
           const check_code =
@@ -51,24 +80,67 @@ app.post("/create-payment-intent", async (req, res) => {
     });
   } else {
     // not js array return error
-    itemsValidated = false;
+    isValidated = false;
   }
+  return isValidated;
+}
 
-  if (itemsValidated) {
-    //Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculateOrderAmount(items),
-      currency: "gbp",
+async function getUserData(auth) {
+  let user = await db.read(null, { boxID: auth.boxID });
+  if (Object.keys(user).length === 1) {
+    user = Object.values(user)[0];
+    if (user.auth.apiKey === auth.apiKey) {
+      return user;
+    } else {
+      // User supplied a valid boxID but not valid apiKey
+      return false;
+    }
+  } else {
+    // If there is no user corresponding to the auth data and if not then create them.
+    return await db.create({ auth: auth, credits: 10 });
+  }
+}
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items, auth } = req.body;
+
+  // input validation
+  let itemsValidated = itemsValidator(items);
+  let authValidated = authValidator(auth);
+
+  if (!itemsValidated) {
+    // error in some input data, return error
+    res.status(400).json({
+      type: "error",
+      message: "Badly formatted payment data",
     });
-
-    res.send({
-      clientSecret: paymentIntent.client_secret,
+  } else if (!authValidated) {
+    // error in some auth data, return error
+    res.status(400).json({
+      type: "error",
+      message: "Badly formatted authorisation data",
     });
   } else {
-    // error in some input data, return error
-    res
-      .status(400)
-      .json({ type: "error", message: "Badly formatted payment data" });
+    // Check that there is a user corresponding to the auth data and if not then create them.
+    const user = await getUserData(auth);
+    if (!user) {
+      // This happens if a user exist but they've not supplied the correct apiKey
+      res.status(401).json({
+        type: "error",
+        message: "Unauthorised request",
+      });
+    } else {
+      //Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "gbp",
+        metadata: { data: JSON.stringify(req.body) },
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    }
   }
 });
 
@@ -76,32 +148,66 @@ app.get("/prices", async (req, res) => {
   res.send(PRICES);
 });
 
-let serverCredits = 10;
 app.post("/credits", async (req, res) => {
-  const clientCredits = req.body.credits;
+  const { credits, auth } = req.body;
 
-  let validated = false;
+  // validation
+  let creditsValidated = false;
+  creditsValidated =
+    typeof credits === "number" && credits % 1 === 0 && credits > 0;
 
-  validated =
-    typeof clientCredits === "number" &&
-    clientCredits % 1 === 0 &&
-    clientCredits > 0;
+  let authValidated = authValidator(auth);
 
-  if (validated) {
-    if (serverCredits > clientCredits) {
-      serverCredits = clientCredits;
-    }
-    res.send({ credits: serverCredits });
+  if (!creditsValidated) {
+    // error in some input data, return error
+    res.status(400).json({
+      type: "error",
+      message: "Badly formatted credits data",
+    });
+  } else if (!authValidated) {
+    // error in some auth data, return error
+    res.status(400).json({
+      type: "error",
+      message: "Badly formatted authorisation data",
+    });
   } else {
-    res
-      .status(400)
-      .json({ type: "error", message: "Badly formatted credits data" });
+    const user = await getUserData(auth);
+    if (!user) {
+      // This happens if a user exist but they've not supplied the correct apiKey
+      res.status(401).json({
+        type: "error",
+        message: "Unauthorised request",
+      });
+    } else {
+      if (credits < user.credits) {
+        // Client has used some credits already. Therefore should update server record of their credits
+        await db.update(user.id, {
+          auth: auth,
+          credits: credits,
+        });
+        res.send({ credits: credits });
+      } else {
+        // Either no credits have been used or user credits are more than recorded on the server which cannot legitimately happen.
+        // In these cases we return the server credits value.
+        res.send({ credits: user.credits });
+      }
+    }
   }
 });
 
-app.post("/" + process.env.PAYMENT_CONFIRMATION, async (req, res) => {
+app.post("/" + process.env.STRIPE_WEBHOOK, async (req, res) => {
   const event = req.body;
-  console.log(event.data.object);
+
+  // TODO: Validate that stripe sent the message
+
+  if (event.type === "payment_intent.succeeded") {
+    const metadata = JSON.parse(event.data.object.metadata.data);
+    const user = await getUserData(metadata.auth);
+
+    const totalCredits = calculateCredits(metadata.items) + user.credits;
+
+    await db.update(user.id, { auth: metadata.auth, credits: totalCredits });
+  }
   res.json({ received: true });
 });
 
